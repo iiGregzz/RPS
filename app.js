7e422dd8b10800254248783dc9ad6b1edb08d888
Greg Wall <wallgr05@outlook.com>
2020-09-30 17:11:00 +0100

    Updated compare function

--- app.js
+++ app.js
@@ -21,31 +21,31 @@ const createAndCollectAiMove = () => {
         return "S";
     }
 }
-alert("AI chose " + createAndCollectAiMove())
+alert("AI chose " + createAndCollectAiMove());
 
 // I need a function that compares the two moves to see who has won the round
 
 const compareAiAndUserScoresToDetermineWhoWins = (userChoice, aiChoice) => {
-    if ((userChoice === "R") && (aiChoice === "R")) {
+    if (userChoice === aiChoice) {
         return "It's a draw!";
-    } else if ((userChoice === "R") && (aiChoice === "R")) {
-        return "It's a draw!";
-    } else if ((userChoice === "P") && (aiChoice === "P")) {
-        return "It's a draw!"
-    } else if ((userChoice === "S") && (aiChoice === "S")) {
-        return "It's a draw!"
-    } else if ((userChoice === "R") && (aiChoice === "S")) {
-        return "Player wins!";
-    } else if ((userChoice === "R") && (aiChoice === "P")) {
-        return "AI wins!";
-    } else if ((userChoice === "P") && (aiChoice === "R")) {
-        return "Player wins!";
-    } else if ((userChoice === "P") && (aiChoice === "S")) {
-        return "AI wins!";
-    } else if ((userChoice === "S") && (aiChoice === "R")) {
-        return "AI wins!";
-    } else {
-        return "Player wins!";
+    } else if (userChoice === "R") {
+        if (aiChoice === "P") {
+            return "AI Wins!";
+        } else {
+            return "Player wins!";
+        }
+        } if (userChoice === "S") {
+            if (computerChoice === "R") {
+            return "AI wins!";
+        } else {
+            return "Player wins!";
+        } 
+        } if (userChoice === "P") {
+            if (computerChoice === "S") {
+            return "AI wins!";
+        } else {
+            return "Player wins!";
+        }
     }
 }
 alert(compareAiAndUserScoresToDetermineWhoWins())
